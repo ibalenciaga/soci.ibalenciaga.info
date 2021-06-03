e3a2b2362e52df5318c64db1c985ae3d0302412d
ibalenciaga <ibalenciaga@gmail.com>
2021-06-03 09:26:07 +0200

    creando la entidad producto. creados los select/create/update de la entidad

---
+++ src/Controller/ProductoController.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Producto;
+use App\Form\ProductoType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ProductoController extends AbstractController
+{
+    /**
+     * @Route("/productos", name="producto")
+     */
+    public function index(): Response{
+        $em = $this->getDoctrine()->getManager();
+        $productos = $em->getRepository(Producto::class)->findAll();
+        return $this->render('producto/index.html.twig', [
+            'productos' => $productos,
+        ]);
+    }
+
+    /**
+     * @Route("/productos/crear", name="crear_producto")
+     */
+    public function crearProducto(Request $request): Response{
+        $producto = new Producto();
+        $form = $this->createForm(ProductoType::class, $producto);
+        $form->handleRequest(($request));
+        if($form->isSubmitted() && $form->isValid()){
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($producto);
+            $em->flush();
+            return $this->redirectToRoute('producto');
+        }
+        return $this->render('producto/crear.html.twig', [
+            'formulario' => $form->createView()
+        ]);
+    }
+
+    /**
+     * @Route("/productos/modificar/{id}", name="modificar_producto")
+     */
+    public function modificarProducto($id, Request $request): Response{
+        $em = $this->getDoctrine()->getManager();
+        $producto = $em->getRepository(Producto::class)->find($id);
+
+        $form = $this->createForm(ProductoType::class, $producto);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em->flush();
+            return $this->redirectToRoute('producto');
+        }
+
+        return $this->render('producto/modificar.html.twig', [
+            'formulario' => $form->createView()
+        ]);
+    }
+}
---
+++ src/Entity/Producto.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ProductoRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=ProductoRepository::class)
+ */
+class Producto
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $nombre;
+
+    /**
+     * @ORM\Column(type="integer", nullable=true)
+     */
+    private $stock;
+
+    /**
+     * @ORM\Column(type="decimal", precision=10, scale=0)
+     */
+    private $precio;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNombre(): ?string
+    {
+        return $this->nombre;
+    }
+
+    public function setNombre(string $nombre): self
+    {
+        $this->nombre = $nombre;
+
+        return $this;
+    }
+
+    public function getStock(): ?int
+    {
+        return $this->stock;
+    }
+
+    public function setStock(?int $stock): self
+    {
+        $this->stock = $stock;
+
+        return $this;
+    }
+
+    public function getPrecio(): ?string
+    {
+        return $this->precio;
+    }
+
+    public function setPrecio(string $precio): self
+    {
+        $this->precio = $precio;
+
+        return $this;
+    }
+}
