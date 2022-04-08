28758b804dc42301efd0b193c39d7987cb037e24
ibalenciaga <ibalenciaga@gmail.com>
2022-04-08 12:44:17 +0200

    nueva reserva - al buscar mesa libre vaciar el div de checkbox-mesa para cargar el listado de mesas libres

--- public/js/find-mesas-libres.js
+++ public/js/find-mesas-libres.js
@@ -1,7 +1,5 @@
 function findMesasLibresAjax(fecha, turno){
     var Ruta = Routing.generate('find_mesas_libres_ajax');
-    console.log(fecha);
-    console.log(turno);
     $.ajax({
         type: "POST",
         url: Ruta,
@@ -9,11 +7,9 @@ function findMesasLibresAjax(fecha, turno){
         async: true,
         dataType: "json",
         success: function (data){
-            console.log(data['mesas']);
             $('.mesa').css("background-color","red");
             if(data['mesas'].length >0){
-                console.log(data['mesas'].length);
-                $("#mesas-libres").append(`<div class="checkbox-mesas">`);
+                $(".checkbox-mesas").html('');
                 $(".checkbox-mesas").append('<label>mesas libres</label>');
                 for (let i = 0; i<data['mesas'].length;i++){
                     campo = '<input type="checkbox" class="checkbox check-mesa" id="mesa-'+i+'" name="mesas[]" value="'+data['mesas'][i]['numero'] +'" data-mesa="'+data['mesas'][i]['numero'] +'">mesa '+data['mesas'][i]['numero'];
